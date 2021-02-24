89fe3f1628e0e433ccf00535585c47e36c28201d
Dawid Cech <dawid.cech@gmail.com>
2021-02-24 18:54:39 +0100

    proposal migration to next.js

---
+++ proposal/pages/index.js
@@ -0,0 +1,50 @@
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from 'react';
+import Proposal from "./proposal"
+
+export default function Proposals() {
+  const [hasError, setErrors] = useState(false)
+  const [index, setIndex] = useState(0)
+  const [count, setCount] = useState(0)
+  const [proposal, setProposal] = useState({})
+  const [proposals, setProposals] = useState([])
+
+  async function fetchData() {
+    const res = await fetch(
+      'https://api.surveyjs.io/private/Surveys/getSurveyResults/83e2dbb2-b4a6-4631-901a-3d92f91725bf?accessKey=dade5a6c382a4252a9bd8dc27eb0e216'
+    )
+
+    res
+      .json()
+      .then((res) => {
+        setProposals(res.Data)
+        setCount(res.ResultCount)
+        setIndex(res.ResultCount - 1)
+        setProposal(res.Data[index])
+      })
+      .catch((err) => setErrors(err))
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const onKeyDown = (e) => {
+    if (e.code === 'ArrowLeft' && index > 0) {
+      e.preventDefault()
+      setIndex(index - 1)
+      setProposal(proposals[index - 1])
+    }
+    if (e.code === 'ArrowRight' && index < count - 1) {
+      e.preventDefault()
+      setIndex(index + 1)
+      setProposal(proposals[index + 1])
+    }
+  }
+
+  return (
+    <div tabIndex="0" style={{ outline: 'none' }} onKeyDown={onKeyDown}>
+      <Proposal {...proposal} />
+    </div>
+  )
+}
---
+++ proposal/pages/proposal.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Proposal(proposal) {
+  return (
+    <div>
+      <h1>Sad rejonowy - {proposal.court} | wydział cywilny</h1>
+      <div style={{ textAlign: 'right' }}>
+        <h3>{proposal.lawyer}</h3>
+        <h3>z Kancelarii (w nagłówku)</h3>
+        <h3>pełnomocnik wnioskodawczyni/wnioskodawcy</h3>
+        {proposal.applicants &&
+          proposal.applicants.map((a) => (
+            <div key={a['applicants.name']}>
+              <h3>{a['applicants.name']}</h3>
+              <h3>{a['applicants.address']}</h3>
+              <h3>PESEL: {a['applicants.pesel']}</h3>
+            </div>
+          ))}
+      </div>
+      <h1 style={{ textAlign: 'center' }}>Wniosek</h1>
+      <h3 style={{ textAlign: 'center' }}>o stwierdzenie nabycia spadku</h3>
+
+      <p>
+        I. Wnoszę o stwierdzenie, że spadek po zmarłej w dniu
+        {proposal['testator.name'] && (
+          <>
+            <i>{proposal['testator.dateOfDeath']}</i>,{' '}
+            <i>{proposal['testator.name']}</i>,
+            <i>
+              {proposal['testator.sex'] === 'female' ? 'córce' : 'synowi'}{' '}
+              {proposal['testator.parents']}
+            </i>
+            , z domu <i>Nowak</i>, ostatnio zamieszkałej w{' '}
+            <i>{proposal['testator.address']}</i>, na mocy dziedziczenia
+            ustawowego, wraz z dobrodziejstwem inwentarza nabyli:
+          </>
+        )}
+      </p>
+      <ul>
+        {proposal.applicants &&
+          proposal.applicants.map((a) => (
+            <li key={a['applicants.name']}>
+              {a['applicants.affinity']} - {a['applicants.name']},{' '}
+              {a['applicants.address']}, PESEL: {a['applicants.pesel']}, udział
+              w masie spadkowej: {a['applicants.inheritence']}
+            </li>
+          ))}
+        {proposal.participants &&
+          proposal.participants.map((p) => (
+            <li key={p['participants.name']}>
+              {p['participants.affinity']} - {p['participants.name']},{' '}
+              {p['participants.address']}, PESEL: {p['participants.pesel']},
+              udział w masie spadkowej: {p['participants.inheritence']}
+            </li>
+          ))}
+      </ul>
+      <p>
+        II. Wnoszę o odebranie na rozprawie od wnioskodawczyni i uczestników
+        oświadczenia o przyjęciu spadku wraz z dobrodziejstwem inwentarza /
+        przyjęciu spadku / odrzuceniu spadku.
+      </p>
+      <p>
+        III. Wnoszę o dopuszczenie dowodu z: a) aktu zgonu spadkobierczyni na
+        fakt daty i miejsca jej śmierci, b) aktu urodzenia wnioskodawczyni i
+        uczestników – na fakt pokrewieństwa ze spadkobierczynią w linii prostej,
+      </p>
+      <p>
+        IV. Wnoszę o orzeczenie w zakresie kosztów, iż każdy z uczestników
+        ponosi koszty postępowania związane ze swym udziałem w sprawie.
+      </p>
+      <h1 style={{ textAlign: 'center' }}>Uzasadnienie</h1>
+      <p>
+        W dniu <i>{proposal['testator.dateOfDeath']}</i> zmarła{' '}
+        <i>{proposal['testator.name']}</i>. W chwili śmierci spadkodawczyni była{' '}
+        <i>{proposal['testator.maritalStatus']}</i> i zamieszkiwała oraz była
+        tymczasowo zameldowana w <i>{proposal['testator.address']}</i>.
+        Spadkodawczyni miała troje dzieci tj. wnioskodawczyni i uczestnicy. Nie
+        pozostawiła po sobie testamentu. W skład spadku nie wchodzi gospodarstwo
+        rolne.
+      </p>
+      <strong>Dowody:</strong>
+      <ul>
+        <li>
+          <i>odpis skrócony aktu zgonu Józefy Kowalskiej,</i>
+        </li>
+        <li>
+          <i>
+            odpis skrócony aktu urodzenia Ewy Kowalskiej, Jana Kowalskiego,
+            Janiny Kowalskiej
+          </i>
+        </li>
+      </ul>
+      <p style={{ marginLeft: '30px' }}>
+        W związku z powyższym wnoszę jak na wstępie.
+      </p>
+      <h3 style={{ textAlign: 'right' }}>{proposal.lawyer}</h3>
+      <strong>Załącznik:</strong>
+      <ul>
+        <li>
+          <i>dwa odpisy wniosku wraz z załącznikami,</i>
+        </li>
+        <li>
+          <i>opłata sądowa w kwocie 100 złotych.</i>
+        </li>
+      </ul>
+    </div>
+  )
+}
